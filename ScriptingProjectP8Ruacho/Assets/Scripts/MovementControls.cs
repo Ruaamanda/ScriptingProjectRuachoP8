1e6a378ffd386eb1bb7bd75da376f6c2bc97b8a6
Ruaamanda <761383@student.dusd.net>
2025-04-04 15:21:10 -0700

    Classes

--- ScriptingProjectP8Ruacho/Assets/Scripts/MovementControls.cs
+++ ScriptingProjectP8Ruacho/Assets/Scripts/MovementControls.cs
@@ -15,7 +15,6 @@ public class MovementControls : MonoBehaviour
     void Movement()
     {
         float forwardMovement = Input.GetAxis("Vertical") * speed * Time.deltaTime;
-        float turnMovement = Input.GetAxis("Horiziontal") * turnSpeed * Time.deltaTime;
         float turnMovement = Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime;
 
         transform.Translate(Vector3.forward * forwardMovement);
